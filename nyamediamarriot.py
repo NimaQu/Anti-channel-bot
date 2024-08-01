0926f8da2c4a681623452d72d3b8d00d6ae1d725
NimaQu <i@nimaqu.com>
2024-08-01 00:03:27 -0500

    add autodelete

--- nyamediamarriot.py
+++ nyamediamarriot.py
@@ -1,3 +1,4 @@
+import asyncio
 from enum import Enum
 from pyrogram import Client
 from pyrogram.types import Message
@@ -44,15 +45,24 @@ class Reservation:
 async def handle_checkin(client: Client, message: Message):
     reservation = reservations.get(message.from_user.id)
     if reservation is None:
-        await message.reply('亲亲找不到您的预定信息哦')
+        r_msg = await message.reply('亲亲找不到您的预定信息哦')
+        await asyncio.sleep(30)
+        await r_msg.delete()
+        await message.delete()
         return
     available_rooms = [room for room in rooms.values() if room.room_status == room_status.available]
     if len(available_rooms) == 0:
-        await message.reply('亲亲没有空房间了哦')
+        r_msg = await message.reply('亲亲没有空房间了哦')
+        await asyncio.sleep(30)
+        await r_msg.delete()
+        await message.delete()
         return
     room = random.choice(available_rooms)
     if reservation.checkin_date > datetime.now(reservation.checkin_date.tzinfo):
-        await message.reply(f'亲亲请在入住日期当天来哦，现在是{datetime.now(cn_tz).strftime("%Y-%m-%d %H:%M:%S")}')
+        r_msg = await message.reply(f'亲亲请在入住日期当天来哦，现在是{datetime.now(cn_tz).strftime("%Y-%m-%d %H:%M:%S")}')
+        await asyncio.sleep(30)
+        await r_msg.delete()
+        await message.delete()
         return
     room.room_status = room_status.occupied
     room.room_member.append(Member(message.from_user.first_name, room.room_number, message.from_user.id))
@@ -64,11 +74,17 @@ async def handle_checkin(client: Client, message: Message):
 async def handle_reservation(client: Client, message: Message):
     args = message.text.split(' ')
     if len(args) < 3:
-        await message.reply('亲亲请按照 /reserve [入住日期] [入住天数] [信用卡卡号] [姓名] [过期时间] [账单地址] [cvv] 的格式预定哦')
+        r_msg = await message.reply('亲亲请按照 /reserve [入住日期] [入住天数] [信用卡卡号] [姓名] [有效期] [账单地址] [cvv] 的格式预定哦')
+        await asyncio.sleep(30)
+        await r_msg.delete()
+        await message.delete()
         return
     if len(args) > 3:
         if args[3] is not None and args[3].startswith('37'):
-            await message.reply('亲亲 no AMEX 哦')
+            r_msg = await message.reply('亲亲 no AMEX 哦')
+            await asyncio.sleep(30)
+            await r_msg.delete()
+            await message.delete()
             return
     checkin_date = datetime.strptime(args[1], '%Y-%m-%d').replace(tzinfo=cn_tz)
     duration_days = timedelta(days=int(args[2]))
@@ -80,7 +96,10 @@ async def handle_checkout(client: Client, message: Message):
     room_number = next((room_number for room_number, room in rooms.items() if
                         any(member.member_id == message.from_user.id for member in room.room_member)), None)
     if room_number is None:
-        await message.reply('亲亲找不到您的房间信息哦')
+        r_msg = await message.reply('亲亲找不到您的房间信息哦')
+        await asyncio.sleep(10)
+        await r_msg.delete()
+        await message.delete()
         return
     room = rooms.get(room_number)
     room.room_status = room_status.available
